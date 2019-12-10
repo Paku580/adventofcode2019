9f38e817d8fc55e256edfb1d362ea59689503468
Paku580 <pascal.kuhlmann2@web.de>
2019-12-10 23:56:42 +0100

    Day 3 Part 2

--- Day03/CrossedWires.py
+++ Day03/CrossedWires.py
@@ -18,37 +18,54 @@ def move_down(point):
     point.move(0, -1)
 
 
-def start():
-    wires = FileReader.read_line_by_line("input.txt")
-    points = []
+def direction_switcher(direction):
     switcher = {
         'R': move_right,
         'L': move_left,
         'U': move_up,
         'D': move_down
     }
+    return switcher.get(direction, lambda: print("ayayay"))
+
+
+def start():
+    wires = FileReader.read_line_by_line("input.txt")
+    points = []
 
     for wire in wires:
-        point = Point(0, 0)
+        point = Point()
         directions = wire.split(',')
         temp_points = []
+        step_count = 0
         for direction in directions:
-            func = switcher.get(direction[0], lambda: print("ayayay"))
+            func = direction_switcher(direction[0])
             direction_length = int(direction[1:])
             for i in range(direction_length):
                 point = Point(point.x, point.y)
                 func(point)
+                step_count += 1
+                point.step_count = step_count
                 temp_points.append(point)
         points.append(temp_points)
 
     common_points = list(set(points[0]).intersection(points[1]))
-    print(common_points)
 
-    closest_intersection = common_points[0].manhattan_distance(Point(0, 0))
+    for i in range(len(common_points)):
+        first_index = points[0].index(common_points[i])
+        second_index = points[1].index(common_points[i])
+
+        common_points[i].step_count = points[0][first_index].step_count + points[1][second_index].step_count
+
+    minimum = min(common_points, key=lambda p: p.step_count)
+
+    closest_intersection = common_points[0].manhattan_distance(Point())
 
     for point in common_points:
-        intersection = point.manhattan_distance(Point(0, 0))
+        intersection = point.manhattan_distance(Point())
         if intersection <= closest_intersection:
             closest_intersection = intersection
 
+    # Part 1
     print(closest_intersection)
+    # Part 2
+    print(minimum.step_count)
--- Day03/Point.py
+++ Day03/Point.py
@@ -1,8 +1,9 @@
 class Point:
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+        self.step_count = 0
 
     def move(self, dx, dy):
         self.x += dx
@@ -12,16 +13,10 @@ class Point:
         return abs(self.x - other_point.x) + abs(self.y - other_point.y)
 
     def __str__(self):
-        return "Point(" + str(self.x) + ", " + str(self.y) + ")"
+        return f"Point{{X: {self.x}, Y: {self.y}, step_count: {self.step_count}}}"
 
     def __repr__(self):
         return self.__str__()
-
-    def get_x(self):
-        return self.x
-
-    def get_y(self):
-        return self.y
 
     def __key(self):
         return self.x, self.y
