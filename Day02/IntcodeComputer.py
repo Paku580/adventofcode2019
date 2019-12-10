bdbb15c719e0dd1addf7415a2f0aaf56a518c07e
Paku580 <pascal.kuhlmann2@web.de>
2019-12-10 10:58:08 +0100

    Day 2 Part 2

--- Day02/IntcodeComputer.py
+++ Day02/IntcodeComputer.py
@@ -3,28 +3,33 @@ from Utils.FileReader import FileReader
 
 class IntcodeComputer:
     intcode = []
-    intcode_pointer = 0
+    instruction_pointer = 0
 
     def __init__(self, intcode):
         self.intcode = intcode
 
     def run_intcode(self):
-        while True:
-            self.switch_opcode(self.intcode[self.intcode_pointer])
+        while self.intcode[self.instruction_pointer] != 99:
+            self.switch_opcode(self.intcode[self.instruction_pointer])
 
     def add(self):
-        self.intcode[self.intcode[self.intcode_pointer + 3]] = self.intcode[self.intcode[self.intcode_pointer + 1]] \
-                                                 + self.intcode[self.intcode[self.intcode_pointer + 2]]
+        self.intcode[self.intcode[self.instruction_pointer + 3]] = self.intcode[
+                                                                       self.intcode[self.instruction_pointer + 1]] \
+                                                                   + self.intcode[
+                                                                       self.intcode[self.instruction_pointer + 2]]
         self.increment_pointer()
 
     def multiply(self):
-        self.intcode[self.intcode[self.intcode_pointer + 3]] = self.intcode[self.intcode[self.intcode_pointer + 1]] \
-                                                 * self.intcode[self.intcode[self.intcode_pointer + 2]]
+        self.intcode[self.intcode[self.instruction_pointer + 3]] = self.intcode[
+                                                                       self.intcode[self.instruction_pointer + 1]] \
+                                                                   * self.intcode[
+                                                                       self.intcode[self.instruction_pointer + 2]]
         self.increment_pointer()
 
     def finish(self):
+        print("noun: " + str(self.intcode[1]))
+        print("verb: " + str(self.intcode[2]))
         print(self.intcode[0])
-        exit()
 
     def switch_opcode(self, opcode):
         opcode = int(opcode)
@@ -37,14 +42,4 @@ class IntcodeComputer:
         func()
 
     def increment_pointer(self):
-        self.intcode_pointer += 4
-
-
-def main():
-    intcode = FileReader.read_comma_separated_values_as_int("input.txt")
-    intcode_computer = IntcodeComputer(intcode)
-    intcode_computer.run_intcode()
-
-
-if __name__ == '__main__':
-    main()
+        self.instruction_pointer += 4
