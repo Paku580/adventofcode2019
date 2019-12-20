f20a77e469af550a09c5a63a8454b55b1d536dad
Paku580 <pascal.kuhlmann2@web.de>
2019-12-20 23:02:45 +0100

    8.1 + 8.2

--- Day08/Solution.py
+++ Day08/Solution.py
@@ -1,30 +1,18 @@
 from Utils.FileReader import FileReader
-import textwrap
-
-
-def find_layer_by_min_occurrences(digit: int) -> int:
-    min_layer = 0
-
-    for index, layer in enumerate(image_data):
-        counts = [row.count(str(digit)) for row in layer]
-        if sum(counts) < count_occurrences(min_layer, digit):
-            min_layer = index
-    return min_layer
-
-
-def multiply(layer: int, multiplicand: int, multiplier: int) -> int:
-    return count_occurrences(layer, multiplicand) * count_occurrences(layer, multiplier)
-
-
-def count_occurrences(layer: int, digit: int) -> int:
-    return sum([row.count(str(digit)) for row in image_data[layer]])
-
 
 if __name__ == '__main__':
     image_data = FileReader.read_line_by_line("input.txt")[0]
     width = 25
     height = 6
-    image_data = textwrap.wrap(image_data, width)
-    image_data = [image_data[i:i + height] for i in range(0, len(image_data), height)]
+    layers = [image_data[i:i + width * height] for i in range(0, len(image_data), width * height)]
+
     # Part 1
-    print(multiply(find_layer_by_min_occurrences(0), 1, 2))
+    layer_with_least_zeroes = min(layers, key=lambda layer: layer.count('0'))
+    print(layer_with_least_zeroes.count('1') * layer_with_least_zeroes.count('2'))
+
+    # Part 2
+    image = [next(layer[i] for layer in layers if layer[i] != '2') for i in range(width * height)]
+    translation_table = {48: ' ', 49: '#'}
+
+    for i in range(0, width*height, 25):
+        print(' '.join(image[i:i+width]).translate(translation_table))
